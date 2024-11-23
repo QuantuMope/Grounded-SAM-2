7d6a4d33f9529a214a3c3eb1355d00bbd40a1ef5
Andrew Choi <neffneff4@gmail.com>
2024-11-22 16:52:35 -0800

    Add missing dependencies to setup.py

--- setup.py
+++ setup.py
@@ -24,11 +24,17 @@ with open("README.md", "r", encoding="utf-8") as f:
 REQUIRED_PACKAGES = [
     "torch>=2.3.1",
     "torchvision>=0.18.1",
-    "numpy>=1.24.4",
+    "numpy==1.26",
     "tqdm>=4.66.1",
     "hydra-core>=1.3.2",
     "iopath>=0.1.10",
     "pillow>=9.4.0",
+    "supervision",
+    "pycocotools",
+    "transformers",
+    "addict",
+    "yapf",
+    "timm",
 ]
 
 EXTRA_PACKAGES = {
